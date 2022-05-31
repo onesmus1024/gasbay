09b1a10b3dc40fa36336f2cc5d7c56fe0087cdfa
onesmus <92239184+onesmus1024@users.noreply.github.com>
2022-05-31 18:40:38 +0300

    Delete globay_key.dart

--- lib/keys/globay_key.dart
+++
@@ -1,5 +0,0 @@
-const consumerKey = 'najyiflPU3AbIocmoiEejvT4lNkPyCCB';
-const consumerSecret = 'CmXOPg3sKcnpmcwJ';
-const googleAPIKkey = "AIzaSyDK6Qwbj57MwzzbJofWjUwhyA1FaDjGPTk";
-const twilioAuthToken = '3713abebb5f088f955547f5444af21fc';
-const twilioAccountSid = 'ACa60cdfcd0b550f8dbc4e9c404fa5cb51';
